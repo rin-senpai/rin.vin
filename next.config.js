7d7c307a685d6a2fa3ea01430e9625d483297e6f
りん <31785428+rin-senpai@users.noreply.github.com>
2023-12-24 16:15:48 +0800

    funni icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    transpilePackages: ['lucide-react']
 }
 
 module.exports = nextConfig
