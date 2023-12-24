703241d58810922af32969b76f6f1a251433a6a2
りん <31785428+rin-senpai@users.noreply.github.com>
2023-12-24 16:02:00 +0800

    wha

---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Riniverse",
+  description:
+    "Seggs",
+  mainNav: [
+    {
+      title: "What",
+      href: "/",
+    },
+    {
+      title: "Seggsering",
+      href: "/seggs",
+    },
+    {
+      title: "Cute and Funny",
+      href: "/uoh",
+    }
+  ]
+}
